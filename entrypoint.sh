df5dfc462b252e247123bd561bdb52ce684d2d15
Ricardo <105866968+RicardoAcosta15@users.noreply.github.com>
2023-07-12 12:23:02 -0400

    nuevo workflow

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-var="./${crud_firebase}/build/outputs/apk/release/*-release.apk"
-local="$(find  -wholename $var)"
-echo $local
-curl -F "file=@$local" \
-        -F "uKey=${uKey}" \
-        -F "_api_key=${apiKey}" \
-        https://www.pgyer.com/apiv1/app/upload
